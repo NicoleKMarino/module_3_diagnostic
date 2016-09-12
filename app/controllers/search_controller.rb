6c3884cc6b2994ca849834efbd1bf5be92da5766
Nicole Marino <nicolekmarino@hotmail.com>
2016-09-12 10:41:26 -0600

    final edits

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-    connection = Faraday.new("https://api.data.gov/nrel")
-    connection.params["api_key"] = "ySKQ5Nl0scz2uT3m3ASrVndOFUgr4YkXBFmi67Zj"
-    connection.params["location"] = params[:q]
-    response = connection.get "/api/alt-fuel-stations/v1/nearest.json"
-    @info = response.body
+    # connection = Faraday.new("https://api.data.gov/nrel")
+    # connection.params["api_key"] = "ySKQ5Nl0scz2uT3m3ASrVndOFUgr4YkXBFmi67Zj"
+    # connection.params["location"] = params[:q]
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=ySKQ5Nl0scz2uT3m3ASrVndOFUgr4YkXBFmi67Zj&location=80027&limit=10&fuel_type=ELEC,LPG")
+    info = parse(response.body)
+    @stations = info[:fuel_stations]
   end
 
 end
